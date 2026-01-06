dc9f68ca552d3dae7461cfb13d516c3590908238
Harsh Sharma <hs210310222360@gmail.com>
2026-01-05 21:36:27 -0500

    content extraction service

--- app/services/llm_services.py
+++ app/services/llm_services.py
@@ -1,6 +1,16 @@
 from ..services.vector_db_services import vector_search_service
 from ..schema import VectorSearchResponse
 
-def extract_content(res: VectorSearchResponse):
-    
-    
+def extract_content(res: VectorSearchResponse) -> list[str]:
+    windows = []
+
+    for result in res.results:
+        content = "\n\n".join(
+            chunk.content for chunk in result.context_chunks
+        )
+        windows.append(content)
+
+    return windows
+
+
+
